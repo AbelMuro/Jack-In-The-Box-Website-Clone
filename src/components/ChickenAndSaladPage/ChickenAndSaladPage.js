23ee1a9c0c981d4424223154e8b073dfae47b7f4
AbelMuro <abelmuro93@gmail.com>
2022-09-03 00:38:50 -0700

    the text was making some of the colums bigger than the others

--- src/components/ChickenAndSaladPage/ChickenAndSaladPage.js
+++ src/components/ChickenAndSaladPage/ChickenAndSaladPage.js
@@ -7,12 +7,32 @@ function ChickenAndSaladPage(){
     <div className={styles.container}>
         <div className={styles.title}>CHICKEN {"&"} SALADS</div>
         <div className={styles.gridItem}>
-            <img src={images["Chicken_Club_Salad"]} className={styles.foodImage}/>    
-            <p>CHICKEN CLUB SALAD</p>        
+            <img src={images["Cluck_Sandwich"]} className={styles.foodImage}/>   
+            <p>CLUCK SANDWICH</p>        
+        </div>
+        <div className={styles.gridItem}>
+            <img src={images["Jacks_Spicy_Chicken"]} className={styles.foodImage}/>
+            <p>JACKS SPICY CHICKEN</p>
+        </div>
+        <div className={styles.gridItem}>
+            <img src={images["Jacks_Spicy_Chicken_Cheese"]} className={styles.foodImage}/>
+            <p>JACKS SPICY CHICKEN CHEESE</p>
+        </div>
+        <div className={styles.gridItem}>
+            <img src={images["Homestyle_Ranch"]} className={styles.foodImage}/>
+            <p>HOMESTYLE RANCH</p>
+        </div>
+        <div className={styles.gridItem}>
+            <img src={images["Chicken_Strips"]} className={styles.foodImage}/> 
+            <p>CHICKEN STRIPS</p>           
         </div>
         <div className={styles.gridItem}>
             <img src={images["Chicken_Nuggets"]} className={styles.foodImage}/> 
             <p>CHICKEN NUGGETS</p>           
+        </div>
+        <div className={styles.gridItem}>
+            <img src={images["Teriyaki_Chicken_Bowl"]} className={styles.foodImage}/>
+            <p>TERIYAKI CHICKEN BOWL</p>
         </div>
         <div className={styles.gridItem}>
             <img src={images["Chicken_Fajita_Pita"]} className={styles.foodImage}/>
@@ -23,40 +43,20 @@ function ChickenAndSaladPage(){
             <p>CHICKEN SANDWICH</p>
         </div>
         <div className={styles.gridItem}>
-            <img src={images["Chicken_Strips"]} className={styles.foodImage}/> 
-            <p>CHICKEN STRIPS</p>           
-        </div>
-        <div className={styles.gridItem}>
-            <img src={images["Cluck_Sandwich"]} className={styles.foodImage}/>   
-            <p>CLUCK SANDWICH</p>        
-        </div>
-        <div className={styles.gridItem}>
-            <img src={images["Grilled_Chicken_Salad"]} className={styles.foodImage}/>
-            <p>GRILLED CHICKEN SALAD</p>        
-        </div>
-        <div className={styles.gridItem}>
-            <img src={images["Homestyle_Ranch"]} className={styles.foodImage}/>
-            <p>HOMESTYLE RANCH</p>
-        </div>
-        <div className={styles.gridItem}>
-            <img src={images["Jacks_Spicy_Chicken_Cheese"]} className={styles.foodImage}/>
-            <p>JACKS SPICY CHICKEN CHEESE</p>
-        </div>
-        <div className={styles.gridItem}>
-            <img src={images["Jacks_Spicy_Chicken"]} className={styles.foodImage}/>
-            <p>JACKS SPICY CHICKEN</p>
-        </div>
-        <div className={styles.gridItem}>
-            <img src={images["Side_Salad"]} className={styles.foodImage}/>
-            <p>SIDE SALAD</p>
+            <img src={images["Chicken_Club_Salad"]} className={styles.foodImage}/>    
+            <p>CHICKEN CLUB SALAD</p>        
         </div>
         <div className={styles.gridItem}>
             <img src={images["Southwest_Chicken_Salad"]} className={styles.foodImage}/>
             <p>SOUTHWEST CHICKEN SALAD</p>
-        </div>
+        </div>  
         <div className={styles.gridItem}>
-            <img src={images["Teriyaki_Chicken_Bowl"]} className={styles.foodImage}/>
-            <p>TERIYAKI CHICKEN BOWL</p>
+            <img src={images["Grilled_Chicken_Salad"]} className={styles.foodImage}/>
+            <p>GRILLED CHICKEN SALAD</p>        
+        </div>      
+        <div className={styles.gridItem}>
+            <img src={images["Side_Salad"]} className={styles.foodImage}/>
+            <p>SIDE SALAD</p>
         </div>
     </div>
     )

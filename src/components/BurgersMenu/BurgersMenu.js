87d000a3d7fe97949506da14edf5118f2ffdccea
AbelMuro <abelmuro93@gmail.com>
2022-09-04 16:21:57 -0700

    finished with the item description component

--- src/components/BurgersMenu/BurgersMenu.js
+++ src/components/BurgersMenu/BurgersMenu.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import images from './images/*.png'
 import styles from './styles.module.css';
 import {Link} from 'react-router-dom';
+import {allDescriptions} from './allDescriptions.js';
 
 function BurgersMenu(){
 
@@ -9,24 +10,26 @@ function BurgersMenu(){
         const choosenItem = e.target.querySelector("div");
         let itemData = {};
         let itemString;
+
         //using string methods to convert "SOURDOUGH JACK" -> "sourdough-jack"
         let itemTitle = choosenItem.querySelector("p").innerHTML.toLowerCase();
-        itemTitle = itemTitle.replace(" ", "-")
+        itemTitle = itemTitle.replaceAll(" ", "-")
+        console.log(itemTitle);
 
         //storing item into the local storage
         itemData["itemImage"] = choosenItem.querySelector("img").getAttribute("src");
         itemData["itemTitle"] = itemTitle;
+        itemData["itemDesc"] = choosenItem.getAttribute("data-desc");
         itemString = JSON.stringify(itemData);
         localStorage.setItem(itemTitle, itemString);
     }
 
     useEffect(() => {
         //keep in mind that all <Link> components get transpiled into <a> tags
-        let allLinks = Array.from(document.querySelectorAll("a"));
+        let allLinks = Array.from(document.querySelectorAll("." + styles.gridItem));
         allLinks.forEach((link) => {
             link.addEventListener("click", storeIntoLocalStorage);
         })
-
         return () => {
             allLinks.forEach((link) => {
                 link.removeEventListener("click", storeIntoLocalStorage);
@@ -38,51 +41,71 @@ function BurgersMenu(){
         <div className={styles.container}> 
             <div className={styles.title}>BURGERS</div>
             <Link to={"/food/sourdough-jack"} className={styles.gridItem}>
-                <div className={styles.eventBubbling}>
+                <div className={styles.eventBubbling} data-desc={allDescriptions.sourdoughJack}>
                     <img src={images["Sourdough_Jack"]} className={styles.foodImage}/>
                     <p>SOURDOUGH JACK</p>
                 </div>
             </Link>
-            <div className={styles.gridItem}>
-                <img src={images["Classic_Buttery_Jack"]} className={styles.foodImage}/> 
-                <p>CLASSIC BUTTERY JACK </p>
-            </div>
-            <div className={styles.gridItem}>
-                <img src={images["Bacon_Buttery_Jack"]} className={styles.foodImage}/>
-                <p>BACON BUTTERY JACK</p>
-            </div>
-            <div className={styles.gridItem}>
-                <img src={images["Double_Jack"]} className={styles.foodImage}/>  
-                <p>DOUBLE JACK</p>
-            </div>
-            <div className={styles.gridItem}>
-                <img src={images["Ultimate_Cheeseburger"]} className={styles.foodImage}/>     
-                <p>ULTIMATE CHEESEBURGER</p>
-            </div>          
-            <div className={styles.gridItem}>
-                <img src={images["Bacon_Ultimate_CheeseBurger"]} className={styles.foodImage}/>
-                <p>BACON ULTIMATE CHEESEBURGER</p>
-            </div>
-            <div className={styles.gridItem}>
-                <img src={images["Jumbo_Jack"]} className={styles.foodImage}/>
-                <p>JUMBO JACK CHEESEBURGER</p>
-            </div>
-            <div className={styles.gridItem}>
-                <img src={images["Jumbo_Jack_NoCheese"]} className={styles.foodImage}/>
-                <p>JUMBO JACK</p>
-            </div>   
-            <div className={styles.gridItem}>
-                <img src={images["Jr_Jumbo_Jack_NoCheese"]} className={styles.foodImage}/> 
-                <p>JR JUMBO JACK</p>
-            </div>  
-            <div className={styles.gridItem}>
-                <img src={images["Jr_Jumbo_Jack"]} className={styles.foodImage}/>    
-                <p>JR JUMBO JACK CHEESEBURGER</p>  
-            </div>      
-            <div className={styles.gridItem}>
-                <img src={images["Jr_Bacon_CheeseBurger"]} className={styles.foodImage}/>
-                <p>JR BACON CHEESEBURGER</p>
-            </div>
+            <Link to={"/food/classic-buttery-jack"} className={styles.gridItem}>
+                <div className={styles.eventBubbling} data-desc={allDescriptions.classicButteryJack}>
+                    <img src={images["Classic_Buttery_Jack"]} className={styles.foodImage}/> 
+                    <p>CLASSIC BUTTERY JACK</p>
+                </div>
+            </Link>
+            <Link to={"/food/bacon-buttery-jack"} className={styles.gridItem}>
+                <div className={styles.eventBubbling} data-desc={allDescriptions.baconButteryJack}>
+                    <img src={images["Bacon_Buttery_Jack"]} className={styles.foodImage}/>
+                    <p>BACON BUTTERY JACK</p>
+                </div>
+            </Link>
+            <Link to={"/food/double-jack"} className={styles.gridItem}>
+                <div className={styles.eventBubbling} data-desc={allDescriptions.doubleJack}>
+                    <img src={images["Double_Jack"]} className={styles.foodImage}/>  
+                    <p>DOUBLE JACK</p>
+                </div>
+            </Link>
+            <Link to={"/food/ultimate-cheeseburger"} className={styles.gridItem}>
+                <div className={styles.eventBubbling} data-desc={allDescriptions.ultimateCheeseburger}>
+                    <img src={images["Ultimate_Cheeseburger"]} className={styles.foodImage}/>     
+                    <p>ULTIMATE CHEESEBURGER</p>
+                </div>
+            </Link>         
+            <Link to={"/food/bacon-ultimate-cheeseburger"} className={styles.gridItem}>
+                <div className={styles.eventBubbling} data-desc={allDescriptions.baconUltimateCheeseburger}>
+                    <img src={images["Bacon_Ultimate_CheeseBurger"]} className={styles.foodImage}/>
+                    <p>BACON ULTIMATE CHEESEBURGER</p>
+                </div>
+            </Link>
+            <Link to={"/food/jumbo-jack-cheeseburger"} className={styles.gridItem}>
+                <div className={styles.eventBubbling} data-desc={allDescriptions.jumboJackCheeseBurger}>
+                    <img src={images["Jumbo_Jack"]} className={styles.foodImage}/>
+                    <p>JUMBO JACK CHEESEBURGER</p>
+                </div>
+            </Link>
+            <Link to={"/food/jumbo-jack"} className={styles.gridItem}>
+                <div className={styles.eventBubbling} data-desc={allDescriptions.jumboJack}>
+                    <img src={images["Jumbo_Jack_NoCheese"]} className={styles.foodImage}/>
+                    <p>JUMBO JACK</p>
+                </div>
+            </Link>   
+            <Link to={"/food/jr-jumbo-jack"} className={styles.gridItem}>
+                <div className={styles.eventBubbling} data-desc={allDescriptions.jrJumboJack}>
+                    <img src={images["Jr_Jumbo_Jack_NoCheese"]} className={styles.foodImage}/> 
+                    <p>JR JUMBO JACK</p>
+                </div>
+            </Link>
+            <Link to={"/food/jr-jumbo-jack-cheeseburger"} className={styles.gridItem}>
+                <div className={styles.eventBubbling} data-desc={allDescriptions.jrJumboJackCheeseburger}>
+                    <img src={images["Jr_Jumbo_Jack"]} className={styles.foodImage}/>    
+                    <p>JR JUMBO JACK CHEESEBURGER</p>  
+                </div>
+            </Link>      
+            <Link to={"/food/jr-bacon-cheeseburger"} className={styles.gridItem}>
+                <div className={styles.eventBubbling} data-desc={allDescriptions.jrBaconCheeseburger}>
+                    <img src={images["Jr_Bacon_CheeseBurger"]} className={styles.foodImage}/>
+                    <p>JR BACON CHEESEBURGER</p>
+                </div>
+            </Link>
         </div>
     )
 }
--- src/components/ItemDescriptionPage/ItemDescriptionPage.js
+++ src/components/ItemDescriptionPage/ItemDescriptionPage.js
@@ -2,29 +2,59 @@ import React from 'react';
 import {useParams} from 'react-router-dom';
 import styles from './styles.module.css';
 import {v4 as uuid} from 'uuid';
+import appStoreButton from './images/appStoreButton.png';
+import googlePlayButton from './images/googlePlayButton.png';
+import jack from './images/jack.png'
 
 function ItemDescription() {
     const {choosenItem} = useParams();
-    const ReactArray = [];    
+    console.log(choosenItem);
+    const titleAndImage = [];  
+    let itemDesc;
     let item = localStorage.getItem(choosenItem);
-
     item = JSON.parse(item);
+
     let modifiedTitle = item.itemTitle.replace("-", " ");
     modifiedTitle = modifiedTitle.toUpperCase();
-    ReactArray.push(<div key={uuid()} className={styles.itemTitle}> {modifiedTitle}</div>); 
-    ReactArray.push(<img key={uuid()} className={styles.itemImage} src={item.itemImage}/>);
+    titleAndImage.push(<div key={uuid()} className={styles.itemTitle}> {modifiedTitle}</div>); 
+    titleAndImage.push(<img key={uuid()} className={styles.itemImage} src={item.itemImage}/>);
+    itemDesc = (<div className={styles.itemDesc}>{item.itemDesc}</div>)
 
     return(
         <div className={styles.itemContainer}>
-            <div className={styles.itemTitleImage}>
-                {ReactArray}
+            <div className={styles.whiteBox}>
+                {titleAndImage}
             </div>
-            <div className={styles.itemInfo}>
-                <div className={styles.itemDesc}>
-                    placeholder
+            <div className={styles.purpleAndRedBox}>
+                <div className={styles.purpleBox}>
+                    {itemDesc}
+                    <div className={styles.nutritionAndIngredients}>
+                        <div>
+                            NUTRITION INFO
+                        </div> 
+                        <div>
+                            INGREDIENTS
+                        </div>
+                    </div>
+                    <div className={styles.notAllMenuItems}>
+                        NOT ALL MENU ITEMS ARE AVAILABLE IN ALL LOCATIONS
+                    </div>
                 </div>
-                <div className={styles.orderNowOnJackApp}>
-                    placeholder
+
+                <div className={styles.redBox}>
+                    <div className={styles.left}>
+                        <div className={styles.title}>
+                            <div className={styles.orderNow}>ORDER NOW</div>
+                            <div className={styles.onTheJackApp}>ON THE JACK APP</div>
+                        </div>
+                        <div className={styles.blackButtons}>
+                            <img src={appStoreButton} className={styles.appStoreButton}/>
+                            <img src={googlePlayButton} className={styles.googlePlayButton}/>
+                        </div>
+                    </div>
+                    <div className={styles.right}>
+                        <img src={jack} className={styles.jackImage}/>
+                    </div>
                 </div>
             </div>
 

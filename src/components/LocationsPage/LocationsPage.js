824eef9e877ef88b13d200aa6d4a753c6961f2e4
AbelMuro <abelmuro93@gmail.com>
2022-09-07 12:02:05 -0700

    started the css for locations page

--- src/components/LocationsPage/LocationsPage.js
+++ src/components/LocationsPage/LocationsPage.js
@@ -4,8 +4,29 @@ import styles from './styles.module.css';
 function LocationsPage(){
 
     return(
-        <main className={styles.locationsContainer}>
-
+        <main className={styles.Container}>
+            <header className={styles.restaurantInfoContainer}>
+                <h1 className={styles.gridItemOne}>
+                    JACK IN THE BOX
+                </h1>
+                <div classname={styles.gridItemTwo}>
+                    <p className={styles.hours}>
+                        HOURS OF OPERATION
+                    </p>
+                    <select className={styles.dropdown}>
+                        <option defaultValue="">6:00 AM - 1:00 AM</option>
+                    </select>
+                </div>
+                <div className={styles.gridItemThree}>
+                    5613 W 38TH ST INDIANAPOLIS, IN 462554
+                    <br/><br/>
+                    {"("}317{")"}291-5703
+                </div>
+                <div className={styles.gridItemFour}>
+                    <button className={styles.blackButtons}> GET DIRECTIONS</button>
+                    <button className={styles.blackButtons}> GET DELIVERY</button>
+                </div>
+            </header>
 
         </main>
     )

e00e02e6a95bf3fb5d0ae7538723582b8ccb0313
AbelMuro <abelmuro93@gmail.com>
2022-09-08 12:07:15 -0700

    almost done with the last component of location page

--- src/components/LocationsPage/MoreInfo/MoreInfo.js
+++ src/components/LocationsPage/MoreInfo/MoreInfo.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import styles from './styles.module.css';
-import weHiring from './styles.module.css';
+import WeHiring from './images/WeHiring.jpg';
+import appStoreButton from './images/appStoreButton.png';
+import googlePlayButton from './images/googlePlayButton.png';
 
 function MoreInfo() {
 
     return(
         <main className={styles.container}>
             <div className={styles.flexItemOne}>
-                <img src={weHiring} className={styles.images}/>
+                <img src={WeHiring} className={styles.images}/>
             </div>
             <div className={styles.flexItemTwo}>
-                <h2>CAREERS</h2>
-                <p>
+                <h2 className={styles.title}>
+                    CAREERS
+                </h2>
+                <p className={styles.description}>
                     Our food is great. Our commercials are funny. 
                     And Jack really does care about you! Feel the
                     pride that only our employees experience by 
                     working at Jack in the Box.
                 </p>
+                <button className={styles.applyButton}>
+                    APPLY TODAY
+                </button>
             </div>
-            <div className={styles.flexItemThree}>1</div>
+            <div className={styles.flexItemThree}>
+                <h2 className={styles.title}>
+                    JACK IN THE BOX MOBILE APP
+                </h2>
+                <p className={styles.description}>
+                    Get all your favorite food delivered from Jack's 
+                    kitchen straight to your door. Order delivery from 
+                    the Jack app today and get everything you're craving 
+                    and more without leaving the comfort of your own home. 
+                    So kick back, relax, we got you covered.
+                </p>
+                <div className={styles.blackButtons}>
+                    <img src={appStoreButton} className={styles.buttonImage}/>
+                    <img src={googlePlayButton} className={styles.buttonImage}/>
+                </div>
+            </div>
             <div className={styles.flexItemFour}>1</div>
             <div className={styles.flexItemFive}>1</div>
             <div className={styles.flexItemSix}>1</div>

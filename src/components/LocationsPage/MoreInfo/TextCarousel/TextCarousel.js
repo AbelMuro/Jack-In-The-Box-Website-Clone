f5ba9b87c64955c8114ba45c8a205c372edea322
abelmuro <abelmuro93@gmail.com>
2022-09-09 19:09:57 -0700

    trying to reduce the lines of code

--- src/components/LocationsPage/MoreInfo/TextCarousel/TextCarousel.js
+++ src/components/LocationsPage/MoreInfo/TextCarousel/TextCarousel.js
@@ -5,28 +5,53 @@ import styles from './styles.module.css';
 
 function TextCarousel() {
 
+    const updateSlideBasedOnDot = (e) => {
+        let clickedDot = e.target;
+        let currentDot = document.querySelector("." + styles.currentDot);   
+        let moveSlideThisAmount = clickedDot.getAttribute("data-position");
+        let slideContainer = document.querySelector("." + styles.flexContainer);        
+        let allSlides = Array.from(document.querySelector("." + styles.flexContainer).children);
+        let currentSlide = document.querySelector("." + styles.currentSlide);     
 
-    const changeDot = () => {
+        currentDot.classList.remove(styles.currentDot);
+        clickedDot.classList.add(styles.currentDot);
+        slideContainer.style.left = moveSlideThisAmount + "px";
+        currentSlide.classList.remove(styles.currentSlide);
+        allSlides.forEach((slide) => {
+            if(slide.getAttribute("data-position") == clickedDot.getAttribute("data-position"))
+                slide.classList.add(styles.currentSlide)
+        })
 
     }
 
+    function updateSlides(currentSlide, nextOrPrevSlide, slideContainer) {
+        slideContainer.style.left = nextOrPrevSlide.getAttribute("data-position") + "px";
+        currentSlide.classList.remove(styles.currentSlide);
+        nextOrPrevSlide.classList.add(styles.currentSlide);
+    }
 
-    const moveSlides = (e) => {
-        let leftOrRight = e.target.classList;
+    function updateDots(currentDot, allDots, nextOrPrevSlide){
+        currentDot.classList.remove(styles.currentDot);
+        allDots.forEach((dot) => {
+            if(dot.getAttribute("data-position") == nextOrPrevSlide.getAttribute("data-position"))
+                dot.classList.add(styles.currentDot)
+        })
+    }
+
+    const updateSlidesAndDots = (e) => {
+        let leftOrRight = e.target.classList;  
         let slideContainer = e.target.parentElement.querySelector("." + styles.flexContainer);
         let currentSlide = slideContainer.querySelector("." + styles.currentSlide);
+        let allDots = Array.from(document.querySelector("." + styles.dotNav).children);
+        let currentDot = document.querySelector("." + styles.currentDot);
 
         if(leftOrRight == styles.right && currentSlide.nextElementSibling != null){
-            let nextSlide = currentSlide.nextElementSibling;
-            slideContainer.style.left = nextSlide.getAttribute("data-position") + "px";
-            currentSlide.classList.remove(styles.currentSlide);
-            nextSlide.classList.add(styles.currentSlide);
+            updateSlides(currentSlide, currentSlide.nextElementSibling, slideContainer);
+            updateDots(currentDot, allDots, currentSlide.nextElementSibling)
         }
         else if(leftOrRight == styles.left && currentSlide.previousElementSibling != null){
-            let prevSlide = currentSlide.previousElementSibling;
-            slideContainer.style.left = prevSlide.getAttribute("data-position") + "px";
-            currentSlide.classList.remove(styles.currentSlide);
-            prevSlide.classList.add(styles.currentSlide);
+            updateSlides(currentSlide, currentSlide.previousElementSibling, slideContainer);
+            updateDots(currentDot, allDots, currentSlide.previousElementSibling);
         }
     }
 
@@ -37,11 +62,11 @@ function TextCarousel() {
             </h2>
             <section className={styles.carouselContainer}>
                 
-                <button className={styles.left} onClick={moveSlides}>
+                <a className={styles.left} onClick={updateSlidesAndDots}>
                     <span className={styles.eventBubbling}>
                         <FontAwesomeIcon icon={faArrowLeft}/>                        
                     </span>
-                </button>
+                </a>
 
                 <div className={styles.window}>
                     <div className={styles.flexContainer}>
@@ -87,15 +112,15 @@ function TextCarousel() {
                     </div>
                 </div>
 
-                <button className={styles.right} onClick={moveSlides}>
+                <a className={styles.right} onClick={updateSlidesAndDots}>
                     <span className={styles.eventBubbling}>
                         <FontAwesomeIcon icon={faArrowRight}/>
                     </span>
-                </button>
+                </a>
                 <nav className={styles.dotNav}>
-                    <div className={styles.dot} onClick={changeDot}></div>
-                    <div className={styles.dot} onClick={changeDot}></div>
-                    <div className={styles.dot} onClick={changeDot}></div>
+                    <div className={[styles.dot, styles.currentDot].join(" ")} onClick={updateSlideBasedOnDot} data-position={0}></div>
+                    <div className={styles.dot} onClick={updateSlideBasedOnDot} data-position={-500}></div>
+                    <div className={styles.dot} onClick={updateSlideBasedOnDot} data-position={-1000}></div>
                 </nav>
 
             </section>

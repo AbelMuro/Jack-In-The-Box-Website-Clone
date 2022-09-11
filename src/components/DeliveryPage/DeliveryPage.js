17c001b476ec8e9105948323fc654dc44e9f1c3d
AbelMuro <abelmuro93@gmail.com>
2022-09-11 11:22:46 -0700

    finished with rewriting this website from scratch!

--- src/components/DeliveryPage/DeliveryPage.js
+++ src/components/DeliveryPage/DeliveryPage.js
@@ -2,6 +2,10 @@ import React from 'react';
 import styles from './styles.module.css'
 import appleAppStore from './images/appleAppStore.png';
 import googlePlayStore from './images/googlePlayStore.png';
+import doordash from './images/doordash.png';
+import ubereats from './images/ubereats.png';
+import grubhub from './images/grubhub.png';
+import postmates from './images/postmates.png';
 
 function DeliveryPage() {
     return(
@@ -25,9 +29,15 @@ function DeliveryPage() {
                     
                 </div>
                 <div className={styles.deliveryPartner}>
-                    <h2 className={styles.deliveryPartner}>
+                    <h2 className={styles.deliveryPartnerTitle}>
                         PICK A DELIVERY PARTNER
                     </h2>
+                    <div className={styles.deliveryApps}>
+                        <img src={doordash} className={styles.appImages}/>
+                        <img src={ubereats} className={styles.appImages}/>
+                        <img src={grubhub} className={styles.appImages}/>
+                        <img src={postmates} className={styles.appImages}/>
+                    </div>
                 </div>
             </div>
         </main>

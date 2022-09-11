9f94afe6e66ae77e3ada290fb7bbaca8b6c706f5
AbelMuro <abelmuro93@gmail.com>
2022-09-10 19:49:52 -0700

    halfway done with the delivery page

--- src/components/DeliveryPage/DeliveryPage.js
+++ src/components/DeliveryPage/DeliveryPage.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import guyLookingAtPhone from './images/guy looking at phone.jpg';
-import munchieBoxMeal from './images/munchie box meal.jpg';
 import styles from './styles.module.css'
+import appleAppStore from './images/appleAppStore.png';
+import googlePlayStore from './images/googlePlayStore.png';
 
 function DeliveryPage() {
     return(
         <main className={styles.container}>
-            <div className={styles.guyLookingAtPhone}>
+            <div className={styles.flexItemOne}>
+                <h1 className={styles.jackDelivered}>
+                    GET JACK <br/>
+                    DELIVERED
+                </h1>
             </div>
-            <div>
+            <div className={styles.flexItemTwo}>
+                <div className={styles.delivery}>
+                    <h1 className={styles.jackApp}>
+                        NOW AVAILABLE <br/>
+                        THROUGH THE JACK APP!
+                    </h1>
+                    <div className={styles.blackButtons}>
+                        <img src={appleAppStore} className={styles.button}/>
+                        <img src={googlePlayStore} className={styles.button}/>
+                    </div>
+                    
+                </div>
+                <div className={styles.deliveryPartner}>
+                    <h2 className={styles.deliveryPartner}>
+                        PICK A DELIVERY PARTNER
+                    </h2>
+                </div>
             </div>
         </main>
     )
